cc83a776a24b48a76603854cd7d9a7ad84f5e512
Petar Todorov <petar.bozhidarov.todorov@gmail.com>
2019-06-26 22:04:18 +0200

    added load dish view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="home">
     <b-container>
-  <b-row>
-    <b-col lg="8"><Slider /></b-col>
-    <b-col lg="4"><StartOrder /></b-col>
-  </b-row>
-  <b-row>
-    <b-col lg="6"><FindOrder /></b-col>
-    <b-col lg="6"><LatestNews /></b-col>
-  </b-row>
-</b-container>
+      <b-row>
+        <b-col lg="8"><Slider /></b-col>
+        <b-col lg="4"><StartOrder /></b-col>
+      </b-row>
+      <b-row>
+        <b-col lg="6"><FindOrder /></b-col>
+        <b-col lg="6"><LatestNews /></b-col>
+      </b-row>
+   </b-container>
 
   </div>
 </template>
---
+++ src/views/LoadDish.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <!-- load dish comp -->
+    <!-- and next  comp-->
+    <b-container>
+      <b-row>
+        <b-col md="8">
+          <img src="../assets/sundown-boulevard.png" alt="dish image">
+          <div id="start-order" class="custom-card">
+            <h4>Dish Title</h4>
+              <p>Dish Description When you are ready go ahead and select your drinks.</p>
+            <div class="text-right">
+              <b-button href="#/load-dish" variant="primary">Generate New</b-button>
+            </div>
+          </div>
+        </b-col>
+        <b-col md="4">
+          <div id="start-order" class="custom-card">
+            <h4>Looks tasty?</h4>
+            <p>When you are ready go ahead and select your drinks.</p>
+            <div class="text-right">
+              <b-button href="#/load-dish" variant="primary">Next</b-button>
+            </div>
+          </div>
+        </b-col>
+      </b-row>
+   </b-container>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'LoadDish',
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+  .container{
+    max-width: 800px;
+  }
+  img{
+    width:100%;
+    height: 300px;
+    object-fit: cover;
+  }
+</style>
+
